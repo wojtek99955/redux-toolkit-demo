add429a5f29e5ee3e8ce6ef73add60665df8948e
Wojtek Książek <wojtek99955@gmail.com>
2022-04-11 20:43:38 +0200

    Action  added to the App component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,33 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addNote } from "./notesSlice";
+
 export default function App() {
-  return <div className="App"></div>;
+  const [value, setValue] = useState();
+  const handleOnChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const notes = useSelector((state) => state.notes.notes); // access the state
+  const dispatch = useDispatch();
+  console.log(notes);
+
+  const save = (e) => {
+    if (value) {
+      return dispatch(addNote(value));
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Add note </h1>
+      <input onChange={handleOnChange} />
+      <button onClick={save}> add </button>
+      <ul>
+        {notes.map((note) => {
+          return <li>{note}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
