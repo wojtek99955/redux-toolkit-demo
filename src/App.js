49cca284a9ba6e5bb2c0f568e2a0cd5e38ab939c
Wojtek Książek <wojtek99955@gmail.com>
2022-04-11 21:58:45 +0200

    Styles update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addNote, deleteLast } from "./notesSlice";
+import { Input, Button } from "@mui/material";
+import { Container, Wrapper, Typography } from "./AppStyles";
 
 export default function App() {
   const [value, setValue] = useState();
@@ -18,16 +20,26 @@ export default function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Add note </h1>
-      <input onChange={handleOnChange} />
-      <button onClick={save}> add </button>
-      <button onClick={() => dispatch(deleteLast())}>usun </button>
+    <Container>
+      <Wrapper>
+        <h1>Add note </h1>
+        <Input onChange={handleOnChange} />
+        <Button variant="contained" onClick={save}>
+          add
+        </Button>
+        <Button
+          variant="outlined"
+          color="error"
+          onClick={() => dispatch(deleteLast())}
+        >
+          delete
+        </Button>
+      </Wrapper>
       <ul>
         {notes.map((note) => {
           return <li>{note}</li>;
         })}
       </ul>
-    </div>
+    </Container>
   );
 }
---
+++ src/AppStyles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  max-width: 800px;
+  margin: auto;
+`;
+export const Wrapper = styled.div`
+  width: 60%;
+  margin: auto;
+
+  h1 {
+    text-align: center;
+    font-size: 3rem;
+  }
+
+  Input {
+    margin-left: auto;
+  }
+
+  Button {
+    width: 5rem;
+    margin-left: 1rem;
+  }
+`;
